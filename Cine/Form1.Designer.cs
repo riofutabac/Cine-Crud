aeea9e7a056a0e2e711f32eb81a6a1bbda33679d
AlexisLapo <alexis.lapo@epn.edu.ec>
2023-06-24 01:26:53 -0500

    Se agregó el Login conectado a una base de datos, falta agregar la funcion del olvidó su contraseña

--- Cine/Form1.Designer.cs
+++ Cine/Form1.Designer.cs
@@ -28,13 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.recordarmeCheckBox = new System.Windows.Forms.CheckBox();
+            this.recuperaContrasenia = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(383, 456);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.White;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Location = new System.Drawing.Point(104, 326);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(182, 49);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(42, 259);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 189);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(75, 263);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(235, 25);
+            this.passwordTextBox.TabIndex = 14;
+            this.passwordTextBox.Text = "Contraseña";
+            this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordTextBox_KeyDown);
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(75, 194);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(235, 25);
+            this.userTextBox.TabIndex = 15;
+            this.userTextBox.Text = "usuario";
+            this.userTextBox.Click += new System.EventHandler(this.userTextBox_Click);
+            this.userTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.userTextBox_KeyDown);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(140, 78);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // recordarmeCheckBox
+            // 
+            this.recordarmeCheckBox.AutoSize = true;
+            this.recordarmeCheckBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.recordarmeCheckBox.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.recordarmeCheckBox.Location = new System.Drawing.Point(75, 299);
+            this.recordarmeCheckBox.Name = "recordarmeCheckBox";
+            this.recordarmeCheckBox.Size = new System.Drawing.Size(102, 21);
+            this.recordarmeCheckBox.TabIndex = 17;
+            this.recordarmeCheckBox.Text = "Recordarme";
+            this.recordarmeCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // recuperaContrasenia
+            // 
+            this.recuperaContrasenia.AutoSize = true;
+            this.recuperaContrasenia.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.recuperaContrasenia.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.recuperaContrasenia.Location = new System.Drawing.Point(127, 378);
+            this.recuperaContrasenia.Name = "recuperaContrasenia";
+            this.recuperaContrasenia.Size = new System.Drawing.Size(143, 17);
+            this.recuperaContrasenia.TabIndex = 18;
+            this.recuperaContrasenia.Text = "¿Olvidó su contraseña?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(341, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 37);
+            this.button1.TabIndex = 19;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(383, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.recuperaContrasenia);
+            this.Controls.Add(this.recordarmeCheckBox);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.pictureBox3);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.CheckBox recordarmeCheckBox;
+        private System.Windows.Forms.Label recuperaContrasenia;
+        private System.Windows.Forms.Button button1;
     }
 }
 
